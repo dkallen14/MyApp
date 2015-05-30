cc91e8465d4730def73ee611e2cde6d6fd96cb47
Daniel Kallen <danieljkallen@gmail.com>
2015-05-29 22:38:39 -0400

    edited views, added a new page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
 	def home
 	end
 
+	def second_page
+	end
+
 	#Testing saving comments
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   root "pages#home"
+  get "/page" => "pages#second_page"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
